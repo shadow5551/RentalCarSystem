723c77a81b608b01037fed6a036d0efeeeeaa41e
Dzmitry <yanusikdima@mail.ru>
2017-03-30 00:28:32 +0300

    DEV3YANUSIK-11

--- RentalCarSysytem/src/main/java/by/training/nc/dev3/interfaces/Reader.java
+++
@@ -1,9 +0,0 @@
-package main.java.by.training.nc.dev3.interfaces;
-
-
-/**
- * Created by dima on 15.3.17.
- */
-public interface Reader {
-    void readItem(String nameOfFile) throws Exception;
-}
--- RentalCarSysytem/src/main/java/by/training/nc/dev3/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package main.java.by.training.nc.dev3.service;
-
-import main.java.by.training.nc.dev3.model.User;
-
-/** Интерфейс служит для добавления объектов класса Order в файл
- */
-public interface UserService {
-    boolean bookingNewCar(User user);
-}
